0bc921d1c1d5d73585890505a714d22ef77ea133
Kevin <kevin.melchert@gmail.com>
2013-05-14 22:14:03 +0200

    single quotes

--- lib/map.rb
+++ lib/map.rb
@@ -11,7 +11,7 @@ class Map
   def get_tile(z, x, y)
     # Convert from ZXY to TMS
     y = (1 << z) - 1 - y
-    db.get_first_value("SELECT tile_data FROM tiles WHERE zoom_level=? AND tile_column=? AND tile_row=?;", z, x, y)
+    db.get_first_value('SELECT tile_data FROM tiles WHERE zoom_level=? AND tile_column=? AND tile_row=?;', z, x, y)
   end  
   
   private
@@ -20,9 +20,9 @@ class Map
     self.name = data['name']
     self.attribution = data['attribution']
     self.version = data['version']
-    lng, lat, default_zoom = data["center"].split(',')
+    lng, lat, default_zoom = data['center'].split(',')
     self.center = { lat: lat.to_f, lng: lng.to_f }
-    self.zoom = { default: default_zoom.to_i, min: data["minzoom"].to_i, max: data["maxzoom"].to_i }
+    self.zoom = { default: default_zoom.to_i, min: data['minzoom'].to_i, max: data['maxzoom'].to_i }
     bounds = data['bounds'].split(',')
     self.bounds = {
       sw: { lat: bounds[1].to_f, lng: bounds[0].to_f },
